072c1a03e76d98257a3d6daf6853ff99bde64b99
Andre Lucas <alucas748@gmail.com>
2022-12-28 20:49:34 -0300

    Criando interface da aplicação

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -12,14 +12,45 @@ class _JogoState extends State<Jogo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("JokenPo"),
+        title: const Text("JokenPo"),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           //text
           //image
           //text resultado
           //row 3 images
+          const Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do app",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Image.asset("assets/images/padrao.png"),
+          const Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Image.asset("assets/images/pedra.png"),
+              Image.asset("assets/images/papel.png"),
+              Image.asset("assets/images/tesoura.png"),
+            ],
+          )
         ],
       ),
     );
